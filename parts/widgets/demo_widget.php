d670e1ed1fd7b3a2ee1e70cf4106409763c6ee74
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-08-09 22:25:13 +0600

    piklist demo widget done

---
+++ parts/widgets/demo_widget.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Title: Test Widget
+Description: A description of what my widget does
+*/
+
+echo $before_widget;
+
+echo $before_title;
+
+echo $settings['demo_text'];
+
+echo $after_title;
+
+echo $settings['demo_select'];
+
+echo $settings['demo_colorpicker'];
+
+echo $after_widget;
---
+++ piklistdemo.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Piklist Widget Demo
+ * Plugin URI: http://saberhr.com
+ * Description: A brief description of the Plugin.
+ * Version: 1.0
+ * Author: SaberHR
+ * Author URI: http://saberhr.com
+ * Plugin Type: Piklist
+ */
